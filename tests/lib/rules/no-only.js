babfd41c0f564705b10298495751ef83b2926769
igoradamenko <mail@igoradamenko.com>
2020-07-03 16:41:19 +0300

    Refactoring

--- tests/lib/rules/no-only.js
+++ tests/lib/rules/no-only.js
@@ -1,10 +1,9 @@
-"use strict";
+const rule = require('../../../lib/rules/no-only');
+const RuleTester = require('eslint').RuleTester;
 
-var rule = require("../../../lib/rules/no-only"),
-    RuleTester = require("eslint").RuleTester;
+const ruleTester = new RuleTester();
 
-var ruleTester = new RuleTester();
-ruleTester.run("no-only", rule, {
+ruleTester.run('no-only', rule, {
   valid: [
     'describe(function() {})',
     'describe.skip(function() {})',
@@ -12,43 +11,50 @@ ruleTester.run("no-only", rule, {
     'describe(function() { it(function() {}); })',
     'obj.only()'
   ],
-  invalid: [{
-      code: "describe.only()",
+  invalid: [
+    {
+      code: 'describe.only()',
       errors: [{
-        message: "Unexpected only",
-        type: "CallExpression",
+        message: 'Unexpected describe.only',
+        type: 'CallExpression',
         line: 1,
         column: 1
       }]
-    }, {
-      code: "it.only()",
+    },
+    {
+      code: 'it.only()',
       errors: [{
-        message: "Unexpected only",
-        type: "CallExpression",
+        message: 'Unexpected it.only',
+        type: 'CallExpression',
         line: 1,
         column: 1
       }]
-    }, {
-      code: "describe(function() {it.only()})",
+    },
+    {
+      code: 'describe(function() {it.only()})',
       errors: [{
-        message: "Unexpected only",
-        type: "CallExpression",
+        message: 'Unexpected it.only',
+        type: 'CallExpression',
         line: 1,
         column: 22
       }]
-    }, {
-      code: "describe.only(function() {it.only()})",
-      errors: [{
-        message: "Unexpected only",
-        type: "CallExpression",
-        line: 1,
-        column: 1
-      }, {
-        message: "Unexpected only",
-        type: "CallExpression",
-        line: 1,
-        column: 27
-      }]
+    },
+    {
+      code: 'describe.only(function() {it.only()})',
+      errors: [
+        {
+          message: 'Unexpected describe.only',
+          type: 'CallExpression',
+          line: 1,
+          column: 1
+        },
+        {
+          message: 'Unexpected it.only',
+          type: 'CallExpression',
+          line: 1,
+          column: 27
+        }
+      ]
     }
   ]
 });
